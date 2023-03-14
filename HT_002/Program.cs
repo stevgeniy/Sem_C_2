cf8d25c81ac1607842b47eb486fbdceb62e10d10
stevgeniy <stepin_evgen@mail.ru>
2023-03-14 22:29:59 +0300

    HT_002

---
+++ HT_002/Program.cs
@@ -0,0 +1,25 @@
+﻿while (true)
+{
+    Console.Write("Введите число : ");
+
+    int num = int.Parse(Console.ReadLine() ?? "");
+
+    if (num >= 100)
+    {
+        Console.WriteLine($"Вы ввели '{num}' ");
+        
+        while (num > 999)
+        {
+            num /= 10;
+        }
+        int Num_3 = num % 10;
+        Console.WriteLine($"Третья цифра числа '{Num_3}' ");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"{num} не имеет третьей цифры, попробуйте еще раз");
+    }
+
+
+}
