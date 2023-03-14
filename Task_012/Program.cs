83185924f7f7c997f56243d3296e70592b5df790
stevgeniy <stepin_evgen@mail.ru>
2023-03-14 21:00:31 +0300

    Task_012

---
+++ Task_012/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число 1: ");
+
+int Num1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите число 2: ");
+
+int Numb2= int.Parse(Console.ReadLine() ?? "");
+
+if (Num1 % Numb2 == 0)
+{
+    Console.Write($"{Num1} Кратно: {Numb2}");
+}
+else
+{
+    Console.Write($"{Num1} не кратно: {Numb2}");
+}
