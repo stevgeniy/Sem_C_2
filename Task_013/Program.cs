c434a3cea36678e506edd2bb676dc7cb20042b59
stevgeniy <stepin_evgen@mail.ru>
2023-03-14 21:08:00 +0300

    Task_013

---
+++ Task_013/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число : ");
+
+int Num = int.Parse(Console.ReadLine() ?? "");
+
+if (Num % 7 == 0 && Num % 23 == 0)
+{
+    Console.Write($"{Num} Кратно 7 и 23");
+}
+else
+{
+    Console.Write($"{Num} не кратно 7 и 23");
+}
