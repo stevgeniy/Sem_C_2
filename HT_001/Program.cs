a1b1ba8249757e09dcad4f139ac929169cdf5918
stevgeniy <stepin_evgen@mail.ru>
2023-03-14 22:08:39 +0300

    HT__While

--- HT_001/Program.cs
+++ HT_001/Program.cs
@@ -1,15 +1,20 @@
-﻿Console.Write("Введите трехзначное число : ");
-
-int num = int.Parse(Console.ReadLine() ?? "");
-
-if (num < 100 || num >= 1000)
+﻿while (true)
 {
-    Console.WriteLine($"{num} не является трёхзначным, попробуйте еще раз");
-    //return;
-}
-else
-{
-    Console.WriteLine($"Вы ввели '{num}' ");
-    int SecondNum = num / 10 % 10;
-    Console.WriteLine($"Вторая цифра числа '{SecondNum}' ");
+    Console.Write("Введите трехзначное число : ");
+
+    int num = int.Parse(Console.ReadLine() ?? "");
+
+    if (num >= 100 && num < 1000)
+    {
+        Console.WriteLine($"Вы ввели '{num}' ");
+        int SecondNum = num / 10 % 10;
+        Console.WriteLine($"Вторая цифра числа '{SecondNum}' ");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"{num} не является трёхзначным, попробуйте еще раз");
+    }
+
+
 }
