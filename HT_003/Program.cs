8ba1201b2245589987a93ffe4183d392236112fb
stevgeniy <stepin_evgen@mail.ru>
2023-03-14 22:39:01 +0300

    HT_003

---
+++ HT_003/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    Console.Write("Введите номер дня : ");
+
+    int day = int.Parse(Console.ReadLine() ?? "");
+
+    if (day > 0 && day <= 7)
+    {
+        if (day > 5)
+        {
+            Console.WriteLine("Выходной день, УРА!");
+        }
+        else
+        {
+            Console.WriteLine("Все на работу!");
+        }
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Такого дня еще не придумали, попробуйте еще раз");
+    }
+
+}
