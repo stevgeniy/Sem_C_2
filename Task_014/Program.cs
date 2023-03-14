3e6c50b949f4ee4ef0f9fdc85c2b5f3c6bace957
stevgeniy <stepin_evgen@mail.ru>
2023-03-14 21:15:30 +0300

    Task_014

---
+++ Task_014/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число 1: ");
+
+int a1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите число 2: ");
+
+int a2= int.Parse(Console.ReadLine() ?? "");
+
+if (a1==a2*a2 || a2==a1*a1)
+{
+    Console.Write($"{a1},{a2} -> Да ");
+}
+else
+{
+    Console.Write($"{a1},{a2} -> Нет ");
+}
