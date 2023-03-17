b66ed3883b4aca0cebfa63f65a13c4728afd5880
stevgeniy <stepin_evgen@mail.ru>
2023-03-18 01:01:08 +0300

    Task_015

---
+++ Task_015/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Clear();
+int num = NumberQuadrat("Введите номер четверти;   ");
+PrintResult(num);
+
+static int NumberQuadrat(string message)
+{
+    int n = 0;
+    while (true)
+    {
+        try
+        { 
+            Console.Write(message);
+            n = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (Exception exc)
+        {Console.WriteLine("Ошибка ввода");}
+    }
+    return n;
+}
+
+static void PrintResult(int num)
+{
+    if (num == 1)
+        Console.WriteLine("x>0 и y>0");
+    else if (num == 2)
+        Console.WriteLine("x>0 и y<0");
+    else if (num == 3)
+        Console.WriteLine("x<0 и y<0");
+    else if (num == 4)
+        Console.WriteLine("x<0 и y>0");
+    else
+        Console.WriteLine("Ошибка! Такой четверти не существует");
+
+}
