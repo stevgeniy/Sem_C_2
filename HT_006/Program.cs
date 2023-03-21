10f42331e739f941d4f2c096e1ed9aead9e6efda
stevgeniy <stepin_evgen@mail.ru>
2023-03-21 23:31:49 +0300

    HT_006_1

--- HT_006/Program.cs
+++ HT_006/Program.cs
@@ -2,7 +2,7 @@
 
     int num = int.Parse(Console.ReadLine() ?? "");
 
-    for (int i = 1; i < num; i++)
+    for (int i = 1; i < num + 1; i++)
     {
         double result = Math.Pow(i, 3);
         Console.Write($"{result}, ");
