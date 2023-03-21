7b9e5a2b84fa765062d3859a847afdfd9f3f206d
stevgeniy <stepin_evgen@mail.ru>
2023-03-21 10:06:17 +0300

    HT_004

---
+++ HT_004/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите натуральное число: ");
+
+string text = Console.ReadLine() ?? "";
+int length = text.Length;
+int[] mas = new int[text.Length];
+
+for (int i = 0; i < length; i++)
+{
+    mas[i] = int.Parse(text[i].ToString());
+}
+
+
+for (int j = 0; j < length / 2; j++)
+{
+    if (mas[j] != mas[length - 1 - j])
+    {
+        Console.WriteLine("НЕ Полиндром");
+        break;
+    }
+
+}
+
+Console.WriteLine("Полиндром");
