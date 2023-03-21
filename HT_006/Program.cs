4bbb772930cbb1f5b11fcd8cf1766c8d4d0df341
stevgeniy <stepin_evgen@mail.ru>
2023-03-21 23:29:00 +0300

    HT_006

---
+++ HT_006/Program.cs
@@ -0,0 +1,9 @@
+﻿ Console.Write("Введите число : ");
+
+    int num = int.Parse(Console.ReadLine() ?? "");
+
+    for (int i = 1; i < num; i++)
+    {
+        double result = Math.Pow(i, 3);
+        Console.Write($"{result}, ");
+    } 
