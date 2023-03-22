f030356862e6b0ba5a92140a43dece39108094dd
stevgeniy <stepin_evgen@mail.ru>
2023-03-22 23:27:17 +0300

    Task_027

---
+++ Task_027/Program.cs
@@ -0,0 +1,31 @@
+﻿int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+int numDigit = GetDigitNum(num);
+Console.WriteLine($"{num} -> содержит {numDigit} цифры");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int usernamber);
+        if (isCorrect && usernamber >= 0)
+        {
+            return usernamber;
+        }  
+        Console.WriteLine(errorMessage);
+
+    }
+    
+}
+
+int GetDigitNum(int num)
+{
+     int count = 0;
+     while (num > 0)
+     {
+        num = num/10;
+        count++;
+     }
+     return count;
+}
