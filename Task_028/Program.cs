8925d1bc094e48629439a6794d3d20482f0b7ea6
stevgeniy <stepin_evgen@mail.ru>
2023-03-22 23:37:45 +0300

    Task_028

---
+++ Task_028/Program.cs
@@ -0,0 +1,35 @@
+﻿int num = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int usernamber);
+        if (isCorrect && usernamber >= 0)
+        {
+            return usernamber;
+        }  
+        Console.WriteLine(errorMessage);
+
+    }
+    
+}
+
+int GetSumNumbers (int number)
+{
+   int sum = 1;
+   while (number > 1)
+   {
+      sum = number*sum;
+      number--;
+   }
+
+   return sum;
+
+
+}
