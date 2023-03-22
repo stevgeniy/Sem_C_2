448ff79d4608fed1deca982ddad9dcd3b5cb368d
stevgeniy <stepin_evgen@mail.ru>
2023-03-23 00:22:10 +0300

    Task_030

---
+++ Task_030/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] array = new int [8];
+FillArray(array);
+//PrintArray(array);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+
+/////////////////////////////////
+
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+   for (int i = 0; i < length; i++)
+   {
+      array[i] = new Random().Next(0,2);
+   }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    Console.Write("[");
+   for (int i = 0; i < length; i++)
+   {
+      Console.Write($"{array[i]}");
+      if (i < length - 1)
+      {
+        Console.Write(", ");
+      }
+   }
+    Console.Write("]");
+}
+
+//  Console.WriteLine($"[{String.Join(",", array)}]");
