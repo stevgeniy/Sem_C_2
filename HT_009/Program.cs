cf94bc53ec98670e82939c2879498276d4cad854
stevgeniy <stepin_evgen@mail.ru>
2023-03-23 07:09:48 +0300

    HT_009

---
+++ HT_009/Program.cs
@@ -0,0 +1,31 @@
+﻿int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+int sumNumDigit = GetSumDigitNum(num);
+Console.WriteLine($"{num} -> сумма цифр {sumNumDigit}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int usernamber);
+        if (isCorrect && usernamber >= 0)
+        {
+            return usernamber;
+        }  
+        Console.WriteLine(errorMessage);
+
+    }
+    
+}
+
+int GetSumDigitNum(int num)
+{
+     int count = 0;
+     while (num > 0)
+     {
+        count += num % 10;
+        num = num/10;        
+     }
+     return count;
+}
