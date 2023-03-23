af1a4fb4ae5d183a94eac20c521ac58fa76217b8
stevgeniy <stepin_evgen@mail.ru>
2023-03-23 06:48:22 +0300

    HT_008

---
+++ HT_008/Program.cs
@@ -0,0 +1,37 @@
+﻿double numA = GetNumberFromUser("Введите число A: ", "Ошибка ввода!");
+double numB = GetNumberFromUser("Введите целое число B: ", "Ошибка ввода!");
+double degreeNumbers = GetDegreeNumbers(numB, numA);
+Console.WriteLine($"{numA}^{numB} -> {degreeNumbers}");
+
+
+
+double GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = double.TryParse(Console.ReadLine(), out double usernamber);
+        if (isCorrect && usernamber >= 0)
+        {
+            return usernamber;
+        }  
+        Console.WriteLine(errorMessage);
+
+    }
+    
+}
+
+double GetDegreeNumbers (double numberB, double numberA)
+{
+   double sum = numberA;
+   while (numberB > 1)
+   {
+      sum *= numberA;
+      numberB--;
+   }
+
+   return sum;
+
+
+}
+
